b98a2e438420745a29cd4b9ad618c395cdf81d63
sky-jo <99069078+sky-jo@users.noreply.github.com>
2023-11-25 17:18:55 -0700

    Create Pair.java

---
+++ Pair.java
@@ -0,0 +1,39 @@
+public class Pair<K, V> {
+	private K key;
+	private V val;
+	private boolean isDeleted;
+
+	public Pair(K key, V val) {
+		this.key = key;
+		this.val = val;
+		this.isDeleted = false;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public V getValue() {
+		return val;
+	}
+
+	public void setValue(V val) {
+		this.val = val;
+	}
+	
+	public void delete() {
+		this.isDeleted = true;
+	}
+	
+	public boolean isDeleted() {
+		return isDeleted; 
+	}
+
+	public boolean equals(Pair p) {
+		return this.key.equals(p.getKey()) && this.val.equals(p.getValue());
+	}
+
+	public String toString() {
+		return "(key = " + key.toString() + ", value = " + val.toString() + ")";
+	}
+}
